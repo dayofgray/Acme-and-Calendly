6e91052005c5bee0c1cbcab625771c23101ce41e
Grayson <graysonpberger@gmail.com>
2019-10-27 19:56:25 -0400

    Update script

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ function answer() {
   var industry = $("#industry :selected").val()
   if (companySize === "smb" || industry === "non-icp")
     window.location.href = "moreinformation.html"
+  else if (email === "grayson@calendly.com")
+    window.location.href = "https://calendly.com/sales-john/30-minute-call-about-acme?name=" + name + "&email=" + email
   else
     window.location.href = "https://calendly.com/sales-team-80/30-minute-acme-call?name=" + name + "&email=" + email
 }
